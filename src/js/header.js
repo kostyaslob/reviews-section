162d68d1e503a9adc6dc7056e89ee769476410c0
Brain-Lady <brain.lady404@gmail.com>
2025-05-03 11:28:12 +0300

    mobile almost ready

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const mobileMenu = document.querySelector('[data-mobile-menu]');
+  const openMenuBtn = document.querySelector('.header-burger-icon'); // Змініть селектор, якщо потрібно
+  const closeMenuBtn = document.querySelector('[data-close-btn]');
 
+  if (openMenuBtn && closeMenuBtn && mobileMenu) {
+    openMenuBtn.addEventListener('click', function () {
+      mobileMenu.classList.add('is-open');
+    });
+
+    closeMenuBtn.addEventListener('click', function () {
+      mobileMenu.classList.remove('is-open');
+    });
+  } else {
+    console.warn('Не вдалося знайти необхідні елементи для мобільного меню.');
+  }
+});
