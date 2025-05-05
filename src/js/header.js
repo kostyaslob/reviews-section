fa6645db0c497e69305609600847fdc41720f5c0
Brain-Lady <brain.lady404@gmail.com>
2025-05-05 16:14:44 +0300

    done header hero modal

--- src/js/header.js
+++ src/js/header.js
@@ -14,3 +14,49 @@ document.addEventListener('DOMContentLoaded', function () {
   } 
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = document.querySelector('[data-modal-window="true"]');
+  const acceptButton = document.querySelector('[data-accept-cookies="true"]');
+  const declineButton = document.querySelector('[data-decline-cookies="true"]');
+
+  
+  const cookiesAccepted = localStorage.getItem('cookiesAccepted');
+
+  if (!cookiesAccepted) {
+    modal.classList.add('is-open');
+  }
+
+
+  acceptButton.addEventListener('click', function () {
+    localStorage.setItem('cookiesAccepted', 'true');
+    modal.classList.remove('is-open');
+  });
+
+ 
+  declineButton.addEventListener('click', function () {
+    localStorage.setItem('cookiesAccepted', 'false');
+    modal.classList.remove('is-open');
+  });
+});
+
+
+ 
+  const headerButtons = document.querySelectorAll('.header-button');
+
+  headerButtons.forEach(button => {
+    button.addEventListener('mousedown', () => {
+      button.classList.add('active');
+    });
+
+    button.addEventListener('mouseup', () => {
+      setTimeout(() => {
+        button.classList.remove('active');
+      }, 3000); 
+    });
+
+    button.addEventListener('mouseleave', () => {
+      button.classList.remove('active');
+    });
+  });
+
+   
